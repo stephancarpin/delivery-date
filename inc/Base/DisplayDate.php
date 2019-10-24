09349875f03c7f772574293dad978c92cce9a034
vivian carpin <viviancarpin@gmail.com>
2019-10-24 12:57:40 +0000

    add save functionality to plugin in post meta

--- inc/Base/DisplayDate.php
+++ inc/Base/DisplayDate.php
@@ -9,9 +9,6 @@ class DisplayDate extends BaseController
     private $cut_off_time ;
     private $holiday_dates ;
     private $normal_delivery_day ;
-    public $output;
-
-
 
     public function register()
     {
@@ -20,13 +17,57 @@ class DisplayDate extends BaseController
 
         add_action( 'woocommerce_cart_totals_after_shipping', array($this,'action_woocommerce_cart_totals_after_shipping'), 30, 0 );
         add_action( 'woocommerce_review_order_before_shipping', array($this,'action_woocommerce_cart_totals_after_shipping'), 25, 0 );
-        add_action( 'woocommerce_email_header', array($this,'action_woocommerce_email'), 10, 0 );
+        add_action( 'woocommerce_email_after_order_table', array($this,'action_woocommerce_email'), 10, 1 );
         add_filter('woocommerce_thankyou_order_received_text',  array($this,'woo_change_order_received_text'), 10, 2 );
 
-
+        //***Save Data in order post**//
+        add_action('woocommerce_review_order_before_payment',  array($this,'custom_checkout_field'), 10, 0 );//but hidden
+        add_action('woocommerce_checkout_process',  array($this,'customised_checkout_field_process'),10,0);
+        add_action('woocommerce_checkout_update_order_meta', array($this,'custom_checkout_field_update_order_meta'),10,1);
+        add_action( 'woocommerce_admin_order_data_after_shipping_address', array($this, 'display_custom_meta_data_in_backend_orders'), 10, 1 );
 
     }
 
+
+    /**
+     * Hide custom field to store Estimated Delivery date
+     */
+    public function custom_checkout_field()
+    {
+        woocommerce_form_field('custom_field_name', array(
+
+            'type'          => 'hidden',
+            'class'         => array('hidden form-row-wide'),
+        ) );
+    }
+
+    /**
+     * Set Output Date to post
+     * Notice if null
+     */
+    public function customised_checkout_field_process()
+    {
+
+        $_POST['customised_field_name'] = sanitize_text_field($this->calculate_delivery_date());
+        // Show an error message if the field is not set.
+        if (!$_POST['customised_field_name']) wc_add_notice(__('Wrong estimated delivery date') , 'error');
+
+    }
+
+    public function custom_checkout_field_update_order_meta($order_id)
+    {
+
+        if (!empty($_POST['customised_field_name'])) {
+
+            update_post_meta($order_id, '_estimated_delivery_date',sanitize_text_field($_POST['customised_field_name']));
+
+        }
+
+    }
+
+    /**
+     * Initialise Field from wordpress
+     */
     public function init()
     {
 
@@ -35,7 +76,6 @@ class DisplayDate extends BaseController
         $this->cut_off_time     =  esc_attr(get_option('cut_off_time'));
         $this->normal_delivery_day     =  esc_attr(get_option('normal_delivery_day'));
         $this->holiday_dates    =  explode(',', esc_attr(get_option('holiday_dates')));
-        $this->output            =   $this->calculate_delivery_date();
 
     }
 
@@ -55,29 +95,53 @@ class DisplayDate extends BaseController
 
     }
 
-    public function action_woocommerce_email( ) {
+    /**
+     * Display in Emails
+     * @param $order
+     */
+    public function action_woocommerce_email($order) {
 
-        $this->init();
+        $from_db_estimated_date =  get_post_meta($order->get_order_number(),'_estimated_delivery_date')[0];
 
-        $html = "<div style='text-align: center;padding-top: 10px;padding-bottom: 10px'>
-                      <h4 style='font-weight: bolder'>Estimated Delivery Date:  " .  $this->calculate_delivery_date()."</h4>
-                 </div>" ;
+        $html = "<p style='margin:0 0 16px'>
+                     <strong>Estimated Delivery Date: </strong> 
+                     ".$from_db_estimated_date."
+                 </p>";
 
 
         echo $html;
 
     }
 
+    /**
+     * Review Order
+     * @param $str
+     * @param $order
+     * @return string
+     */
     public function woo_change_order_received_text( $str, $order ) {
 
-        $this->init();
+        $from_db_estimated_date =  get_post_meta($order->get_order_number(),'_estimated_delivery_date')[0];
 
-        $html = "<div style='text-align: center;padding-top: 10px;padding-bottom: 10px'>
-                     <h4 style='font-weight: bolder'>Estimated Delivery Date:  " .  $this->calculate_delivery_date()."</h4>
+        $html = "<div style='t;padding-top: 10px;padding-bottom: 10px'>
+                     <h4 style='font-weight: bolder'>Estimated Delivery Date:  " .  $from_db_estimated_date ."</h4>
                 </div>" ;
 
 
         return $str . $html;
+    }
+
+
+    /**
+     * To display Estimated date in back office orders
+     * @param $order
+     */
+   public function display_custom_meta_data_in_backend_orders( $order ){
+        $domain = 'woocommerce';
+
+       $from_db_estimated_date =  get_post_meta($order->get_order_number(),'_estimated_delivery_date')[0];
+        if( ! empty( $from_db_estimated_date ) )
+            echo '<p><strong>'.__('Estimated Delivery Date', $domain).': </strong> ' . $from_db_estimated_date . '</p>';
     }
 
     /**
@@ -229,27 +293,41 @@ class DisplayDate extends BaseController
     }
 
     /**
-     *
+     * Retrieve shipping methods base on $shipping_id
+     * @return mixed
      */
     private function get_shipping_methods()
     {
-        $Shipping_method_name = $this->get_shipping_name_by_id( WC()->session->get( 'chosen_shipping_methods' )[0] );
-       // self::p($Shipping_method_name);
+        $shipping_id = WC()->session->get( 'chosen_shipping_methods' )[0];
+        global $woocommerce;
+
+        $Shipping_method_name = $woocommerce->session->shipping_for_package_0['rates'][$shipping_id]->label;
         return $Shipping_method_name;
-
     }
+
+    /** to delete
+     * @param $shipping_id
+     * @return mixed
+     */
     public  function get_shipping_name_by_id( $shipping_id ) {
-        $packages = WC()->shipping->get_packages();
 
-        foreach ( $packages as $i => $package ) {
-            if ( isset( $package['rates'] ) && isset( $package['rates'][ $shipping_id ] ) ) {
-                $rate = $package['rates'][ $shipping_id ];
-                /* @var $rate WC_Shipping_Rate */
-                return $rate->get_label();
-            }
-        }
+//        $packages = WC()->shipping->get_packages();
+//        foreach ( $packages as $i => $package ) {
+//            if ( isset( $package['rates'] ) && isset( $package['rates'][ $shipping_id ] ) ) {
+//               // self::p($package['rates']);
+//                $rate = $package['rates'][ $shipping_id ];
+//                /* @var $rate WC_Shipping_Rate */
+//                return $rate->get_label();
+//            }
+//        }
 
-        return '';
+
+        global $woocommerce;
+
+        $packages = $woocommerce->session->shipping_for_package_0['rates'][$shipping_id]->label;
+
+        self::p($packages);
+        return $packages;
     }
 
     public function p($value)
